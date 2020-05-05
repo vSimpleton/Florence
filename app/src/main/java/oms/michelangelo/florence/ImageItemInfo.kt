5516a81affd4a97694ac16051ca90c75e7835920
vSimpleton <masm@luxuewen.com>
2020-05-05 21:49:24 +0800

    :rocket: change java to kt

---
+++ app/src/main/java/oms/michelangelo/florence/ImageItemInfo.kt
@@ -0,0 +1,14 @@
+package oms.michelangelo.florence
+
+/**
+ * Created by Sherry on 2020/5/5
+ * 获取到的图片实体类
+ */
+
+class ImageItemInfo {
+
+    var name: String? = null
+    var path: String? = null
+    var createTime: Long? = 0
+
+}
