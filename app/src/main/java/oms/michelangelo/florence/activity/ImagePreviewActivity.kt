2b1daae572e15cc4db1215f79e91897cc14f96b7
vSimpleton <masm@luxuewen.com>
2020-05-18 00:08:48 +0800

    :pencil2: create imagepreview

---
+++ app/src/main/java/oms/michelangelo/florence/activity/ImagePreviewActivity.kt
@@ -0,0 +1,13 @@
+package oms.michelangelo.florence.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import oms.michelangelo.florence.R
+
+class ImagePreviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_preview)
+    }
+}
--- app/src/main/java/oms/michelangelo/florence/activity/MainActivity.kt
+++ app/src/main/java/oms/michelangelo/florence/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -19,16 +20,10 @@ import oms.michelangelo.florence.adapter.SelectPhotoAdapter
 import oms.michelangelo.florence.utils.MediaManager.Callback
 import oms.michelangelo.florence.utils.StatusBarUtil
 
-private val REQUIRED_PERMISSIONS =
-    arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+class MainActivity : AppCompatActivity(), View.OnClickListener{
 
-class MainActivity : AppCompatActivity() {
-
-    private var list: MutableList<String> = ArrayList()
-    private val REQUEST_PERMISSION_CODE = 100
     private lateinit var mAdapter: SelectPhotoAdapter
     private lateinit var mLists: ArrayList<MediaData>
-    private var hasPermissionDenied = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +35,14 @@ class MainActivity : AppCompatActivity() {
         initPermission()
         initRecyclerView()
         getAllImageList()
+        initListener()
 
+    }
+
+    private fun initListener() {
+        tvPreview.setOnClickListener(this)
+        tvComplete.setOnClickListener(this)
+        ivBack.setOnClickListener(this)
     }
 
     private fun initRecyclerView() {
@@ -70,6 +72,20 @@ class MainActivity : AppCompatActivity() {
                 getAllImageList()
             } else {
                 Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            ivBack  -> {
+                finish()
+            }
+            tvPreview -> {
+                
+            }
+            tvComplete -> {
+
             }
         }
     }
